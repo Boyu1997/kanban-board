06175b3c09f771a738b34f562afacc9ebaf8f287
Boyu Jiang <jiangby97@gmail.com>
2019-03-06 14:13:14 +0800

    feat(frontend): sort card in list by draggin

--- frontend/src/ListCards.js
+++ frontend/src/ListCards.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react'
+import HTML5Backend from 'react-dnd-html5-backend'
+import { DragDropContext } from 'react-dnd'
 import Card from './Card.js'
 import './ListCards.css';
+const update = require('immutability-helper');
 
 class ListCards extends Component {
+  state = {
+    cards: [],
+  }
+  moveCard = (dragIndex, hoverIndex) => {
+    const { cards } = this.state
+    const dragCard = cards[dragIndex]
+
+    this.setState(
+      update(this.state, {
+        cards: {
+          $splice: [[dragIndex, 1], [hoverIndex, 0, dragCard]],
+        },
+      }),
+    )
+  }
+
+  componentDidMount() {
+    this.setState({ cards: this.props.cards})
+  }
   render() {
     return (
       <div className="list-cards">
         <h1>{this.props.category}</h1>
-        {this.props.cards.map((card) => (
+        {this.state.cards.map((card, i) => (
           <Card
-            key={card.id}
-            title={card.title}
+            key = {card.id}
+            index = {i}
+            id = {card.id}
+            title = {card.title}
+            moveCard = {this.moveCard}
           />
         ))}
       </div>
@@ -18,4 +43,4 @@ class ListCards extends Component {
   }
 }
 
-export default ListCards
+export default DragDropContext(HTML5Backend)(ListCards);
