370e8eddd348a1769dbfcd1e2574fa70b20c3e13
Boyu Jiang <jiangby97@gmail.com>
2019-03-18 19:18:24 -0300

    feat(frontend): change delete button to icon

--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './style/Card.css';
+import trashIcon from './icon/icons8-trash.svg';
 
 
 class Card extends Component {
@@ -11,7 +12,7 @@ class Card extends Component {
           opacity: isDragging ? 0.3 : 1,
         }}>
         <p>{card.title}</p>
-        <button className='delete-button' onClick={deleteCardHandler}>Delete</button>
+    	  <img src={trashIcon} onClick={deleteCardHandler} alt="Delete"/>
       </div>
     )
   }
